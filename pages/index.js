5bd8d546c79b355f45d9a7852cb36fafc4442278
Chris Kim <chriskim@cohere.com>
2023-05-04 16:09:18 -0400

    pushhhh

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi👋 I'm Chris!]</p>
+        <p>[Hi👋 I'm Chabs!]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I'm a cat🐱
         </p>
       </section>
 
